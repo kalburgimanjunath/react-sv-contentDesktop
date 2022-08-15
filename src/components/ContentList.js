33f8c82f3e46cca91c68dd15832e76f691afea29
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2022-08-15 21:47:02 +0530

    1 file modified

--- src/components/ContentList.js
+++ src/components/ContentList.js
@@ -1,37 +1,38 @@
 import React from 'react';
 // import { data } from '../data/data.js';
-const fetch = require('node-fetch');
-const getVoiceData = () => {
-  const url = 'https://api.lovo.ai/v1/conversion';
-  const data = {
-    text: 'hello world! my name is Martha Sage',
-    speaker_id: 'Martha Sage',
-    emphasis: '[0, 5]',
-    speed: 1,
-    pause: 0,
-    encoding: 'mp3',
-  };
-  const option = {
-    method: 'POST',
-    headers: {
-      apiKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZmEyNmRlM2JmNzMzMDAxMjY2MjUwMyIsImlhdCI6MTY2MDU2MTEyMTcxM30.sdLz_6D4DZSbg6cXObNRrKiwTNA47HQSRh51zCaO1Jw', // Your API key goes here
-      'Content-Type': 'application/json; charset=utf-8',
-    },
-    body: JSON.stringify(data),
-  };
+// const fetch = require('node-fetch');
+// import fetch from 'node-fetch';
+// const getVoiceData = () => {
+//   const url = 'https://api.lovo.ai/v1/conversion';
+//   const data = {
+//     text: 'hello world! my name is Martha Sage',
+//     speaker_id: 'Martha Sage',
+//     emphasis: '[0, 5]',
+//     speed: 1,
+//     pause: 0,
+//     encoding: 'mp3',
+//   };
+//   const option = {
+//     method: 'POST',
+//     headers: {
+//       apiKey:
+//         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZmEyNmRlM2JmNzMzMDAxMjY2MjUwMyIsImlhdCI6MTY2MDU2MTEyMTcxM30.sdLz_6D4DZSbg6cXObNRrKiwTNA47HQSRh51zCaO1Jw', // Your API key goes here
+//       'Content-Type': 'application/json; charset=utf-8',
+//     },
+//     body: JSON.stringify(data),
+//   };
 
-  fetch(url, option)
-    .then((res) => res.arrayBuffer())
-    .then((buffer) => {
-      //output filename goes below
-      console.log('sample created');
-      fs.appendFileSync('sample.mp3', Buffer.from(buffer));
-    })
-    .catch((error) => {
-      // error handling
-    });
-};
+//   fetch(url, option)
+//     .then((res) => res.arrayBuffer())
+//     .then((buffer) => {
+//       //output filename goes below
+//       console.log('sample created');
+//       fs.appendFileSync('sample.mp3', Buffer.from(buffer));
+//     })
+//     .catch((error) => {
+//       // error handling
+//     });
+// };
 
 export default function ContentList({ data }) {
   // console.log(data);
@@ -41,9 +42,7 @@ export default function ContentList({ data }) {
         <div>
           Content:
           {item.title.toUpperCase()}
-          <button type="button" onClick={getVoiceData()}>
-            Play
-          </button>
+          <button type="button">Play</button>
         </div>
         <div>Language:{item.language}</div>
       </div>
